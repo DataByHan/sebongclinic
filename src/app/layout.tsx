7b9b9a4346b4e8f58adae9d3e10c6d6ce13e66e5
hanms.data <hanminseok@example.com>
2026-04-07 13:15:12 +0900

    refactor(analytics): add GTM comment for clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,7 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+
 }
 
 export const viewport: Viewport = {
@@ -121,13 +122,10 @@ export default function RootLayout({
   return (
     <html lang="ko" className={[sans.variable, serif.variable].join(' ')}>
       <head>
+        {/* Google Tag Manager - Must be first in head */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${gtmId}');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`,
           }}
         />
       </head>
